ce4b0e5f7c7fa725cf53c1d78eed35eeeb483e36
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-12-09 04:56:02 -0500

    Fixes for src

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { createWorker } from "tesseract.js";
 import { SingleBar, Presets } from "cli-progress";
-// import Upscaler from "upscaler";
+import fs from 'fs';
+import * as tf from '@tensorflow/tfjs-node';
+const Upscaler = require('upscaler/node');
 
 (async () => {
-  // const upscaler = new Upscaler(/*{ scale: 2 }*/);
+  const upscaler = new Upscaler();
   const recognizeBar = new SingleBar({}, Presets.shades_classic);
   const worker = await createWorker({
     logger: m => recognizeBar.update(m.progress)
@@ -13,13 +15,18 @@ import { SingleBar, Presets } from "cli-progress";
   const file = process.argv[2] || "";
   const lang = 'fra';
 
-  // console.log(`Opening ${file}`)
-  // const image = await upscaler.upscale(file);
-  const image = file;
+  console.log(`Opening ${file}`)
+  const image = tf.node.decodeImage(fs.readFileSync(file), 3);
+  const upscaledTensor = await upscaler.upscale(image, {
+    output: 'tensor',
+  });
+  const upscaledImage = await tf.node.encodePng(upscaledTensor);
+  fs.writeFileSync('output.png', upscaledImage);
+
   await worker.loadLanguage(lang);
   await worker.initialize(lang);
   recognizeBar.start(1.0, 0)
-  const { data: { text } } = await worker.recognize(image);
+  const { data: { text } } = await worker.recognize('output.png');
   recognizeBar.stop();
   console.log(text);
   await worker.terminate();
